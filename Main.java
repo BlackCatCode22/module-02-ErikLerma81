0978db3eaaa5b8e1dbc06a0448d329bf0f0d0657
ErikLerma81 <124100945+ErikLerma81@users.noreply.github.com>
2024-02-04 22:33:40 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,46 @@
 import java.util.Scanner;
 public class Main {
+    static void getAnIntFromUser(int num) {
+        System.out.println("Your number is " + num);
+    }
 
-
-    static void LargestOfThree (int num1, int num2, int num3){
-        int largest = 0;
-        if (num1 > num2 && num1 > num3){
-            largest = num1;
+    static void compareTwoInts(int num1, int num2){
+        if (num1 > num2){
+            System.out.println(num1 + " is the largest number");
         }
-        if (num2 > num1 && num2 > num3){
-            largest = num2;
-        }
-        if (num3 > num1 && num3 > num2){
-            largest = num3;
+        else{
+            System.out.println(num2 + " is the largest number");
         }
 
-        System.out.print("Largest number of the 3 is " + largest);
+    }
+
+    static void sumTwoints(int num1, int num2){
+        int total = num1 + num2;
+        System.out.println(total + " is the sum of " + num1 + " and " + num2);
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter 3 Numbers: ");
+        System.out.println("Enter a Number: ");
         Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+
+        getAnIntFromUser(number);
+
+        System.out.println("Enter 2 Numbers to compare: ");
+
+
+        int numb1 = scanner.nextInt();
+        int numb2 = scanner.nextInt();
+
+        compareTwoInts(numb1, numb2);
+
+
+        System.out.println("Enter 2 Numbers to get the sum: ");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
-        int num3 = scanner.nextInt();
 
-        LargestOfThree(num1, num2, num3);
-
-
-
+        sumTwoints(num1, num2);
 
 
     }
